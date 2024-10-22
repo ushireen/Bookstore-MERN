4397ca47ce237f49a8dfdd1bb8530f2dbc6f2616
Uzma Shireen <uzsh6413@gmail.com>
2024-10-22 12:17:56 +0530

    BookStore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,31 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CreateBook from "./pages/CreateBook";
 import ShowBook from "./pages/ShowBookBook";
 import EditBook from "./pages/EditBook";
 import Home  from "./pages/Home";
 import DeleteBook from "./pages/DeleteBook";
 
-export default function App() {
-  
-  // useEffect(() => {
+const  App = () => {
+      return (    
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/books/create" element={<CreateBook />}/>
+        <Route path="/books/details/:id" element={<ShowBook />}/>
+        <Route path="/books/edit/:id" element={<EditBook />}/>
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+        
+      </Routes>
+     
+  );
+};
+export default App;
+
+
+// useEffect(() => {
   //   axios
   //   .get("http://localhost:3000/books")
   //   .then((response) => console.log(response))
   //   .catch((error) => console.error("Error fetching data", error));
   // }, []);
-
-  return (
-    <>  
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} /> 
-        <Route path="/books/create" element={<CreateBook/>}/>
-        <Route path="/books/details" element={<ShowBook/>}/>
-        <Route path="/books/edit" element={<EditBook/>}/>
-        <Route path="/books/delete" element={<DeleteBook/>} />
-        
-      </Routes>
-     </Router>
-
-
-    </>
-  )
-}
-
